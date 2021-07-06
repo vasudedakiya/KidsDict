6201ca3fbb44af01cecf25300d3d5087e18fce87
Vasu <vasudedakiya1742002@gmail.com>
2021-07-07 03:12:32 +0530

    backup 7/7/21 3:21 AM

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LevelsComponent } from './levels/levels.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "level/:id", component: LevelsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
