869d22461c07fa2858f6718666a21cc9e11b2924
Vasu <vasudedakiya1742002@gmail.com>
2021-07-15 00:55:16 +0530

    backup

--- src/app/level1/level1.component.ts
+++ src/app/level1/level1.component.ts
@@ -8,24 +8,52 @@ import { Router } from '@angular/router';
 })
 export class Level1Component implements OnInit {
 
-  temp1 = "../../assets/images/Spelling_Button/SpelingButton1.webp";
-  temp2 = "../../assets/images/Spelling_Button/SpelingButton2.webp";
-  temp3 = "../../assets/images/Spelling_Button/SpelingButton3.webp";
-  temp4 = "../../assets/images/Spelling_Button/SpelingButton4.webp";
-  temp5 = "../../assets/images/Spelling_Button/SpelingButton5.webp";
-  temp6 = "../../assets/images/Spelling_Button/SpelingButton6.webp";
-  temp7 = "../../assets/images/Spelling_Button/SpelingButton7.webp";
-  temp8 = "../../assets/images/Spelling_Button/SpelingButton8.webp";
 
 
+  demo: { name: string, latter: any, url: string }[] = [
+    { name: "apple", latter: "apple".split(''), url: "../../assets/images/Alphabets/A.webp" },
+    { name: "ball", latter: "ball".split(''), url: "../../assets/images/Alphabets/B.webp" },
+    { name: "cat", latter: "cat".split(''), url: "../../assets/images/Alphabets/C.webp" },
+    { name: "dog", latter: "dog".split(''), url: "../../assets/images/Alphabets/D.webp" },
+  ]
 
+  index: number = 0;
+  imgurl: string = ""
+  data: { url: string, latter: string }[] = []
   constructor(private _route: Router) { }
 
   ngOnInit(): void {
+    this.displaydata();
   }
 
   goBack() {
     this._route.navigate(['level', 1])
   }
+
+  prev() {
+    this.index = this.index - 1;
+    this.data = [];
+    this.displaydata();
+  }
+
+  next() {
+    this.index = this.index + 1;
+    this.data = [];
+    this.displaydata()
+  }
+
+  displaydata() {
+    for (let i = 0; i < this.demo[this.index].latter.length; i++) {
+      let temp = {
+        url: "../../assets/images/Spelling_Button/SpelingButton" + (i + 1) + ".webp",
+        latter: this.demo[this.index].latter[i],
+      }
+      this.data.push(temp);
+    }
+    this.imgurl = this.demo[this.index].url;
+  }
+
+
+
 
 }
