80721975fbcd74e032384b91630567b96d83627a
Vasu <vasudedakiya1742002@gmail.com>
2021-07-18 01:02:42 +0530

    level 2 & 4 desing complite

--- src/app/level4/level4.component.ts
+++ src/app/level4/level4.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-level4',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Level4Component implements OnInit {
 
-  constructor() { }
+  constructor(private _route: Router) { }
+
+  demo: { name: string, latter: any, url: string }[] = [
+    { name: "apple", latter: "apple".split(''), url: "../../assets/images/Alphabets/A.webp" },
+    { name: "ball", latter: "ball".split(''), url: "../../assets/images/Alphabets/B.webp" },
+    { name: "cat", latter: "cat".split(''), url: "../../assets/images/Alphabets/C.webp" },
+    { name: "dog", latter: "dog".split(''), url: "../../assets/images/Alphabets/D.webp" },
+  ]
+
+
+  index: number = 0;
+  imgurl: string = ""
+  data: { url: string, latter: string }[] = []
 
   ngOnInit(): void {
+    this.displaydata();
+  }
+
+  goBack() {
+    this._route.navigate(['level', 4])
+  }
+
+  changeSlid(id: any) {
+    this.index = this.index + id;
+    this.data = [];
+    this.ngOnInit();
+  }
+
+  displaydata() {
+    for (let i = 0; i < this.demo[this.index].latter.length; i++) {
+      let temp = {
+        url: "../../assets/images/Spelling_Button/SpelingButton" + (i + 1) + ".webp",
+        latter: this.demo[this.index].latter[i],
+      }
+      this.data.push(temp);
+
+    }
+    this.imgurl = this.demo[this.index].url;
   }
 
 }
