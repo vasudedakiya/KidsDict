0c124b178f428af198a7da0e328924960bebe7a7
Vasu <vasudedakiya1742002@gmail.com>
2021-07-26 11:08:33 +0530

    level 1,2,4 compelete

--- src/app/level3/level3.component.ts
+++ src/app/level3/level3.component.ts
@@ -11,6 +11,20 @@ export class Level3Component implements OnInit {
   constructor(private _api: ApiKidsDictService) { }
 
   ngOnInit(): void {
+    console.log(this.makeid(5));
   }
+
+
+  makeid(length: number) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() *
+        charactersLength));
+    }
+    return result;
+  }
+
 
 }
